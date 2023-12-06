dde49765b68427f499f99d599a21f8a9f321cf5e
--help <papertrack@outlook.com>
2023-12-06 16:32:50 +0800

    complete: D7

---
+++ Day01-15/test.py
@@ -0,0 +1,20 @@
+def get_suffix(filename, has_dot=False):
+    """
+    获取文件名的后缀名
+
+    :param filename: 文件名
+    :param has_dot: 返回的后缀名是否需要带点
+    :return: 文件的后缀名
+    """
+    pos = filename.rfind('.')
+    # pos = filename.find('.')
+    if 0 < pos < len(filename) - 1:
+        index = pos if has_dot else pos + 1
+        return filename[index:]
+    else:
+        return ''
+
+
+if __name__ == "__main__":
+    suff = get_suffix('fdf/fdfa.file.txt', has_dot=False)
+    print(suff)
